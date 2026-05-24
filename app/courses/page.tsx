a00828a218ce99020d3bc5b11e8ec56bfcee43fd
Kshitij <kshitijgundewae12345@gmail.com>
2026-05-24 14:58:53 +0530

    added courses page

---
+++ app/courses/page.tsx
@@ -0,0 +1,9 @@
+export default function Courses() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Courses Page
+      </h1>
+    </div>
+  )
+}
